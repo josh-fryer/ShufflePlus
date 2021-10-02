2ef1eef6c7a5bfc0c7cdced0a629084370876085
Josh Fryer <josh.fryer2@gmail.com>
2021-10-02 02:37:13 +0100

    refresh access token when it expires.

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ global.expires_in = 0;
 
 dotenv.config();
 
-var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
-var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+var spotify_client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+var spotify_client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 
 var spotify_redirect_uri = "http://localhost:3000/auth/callback";
 
--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
 import React, { useState, useEffect } from "react";
 import WebPlayback from "./WebPlayback";
 import Login from "./Login";
-const webpack = require("webpack");
 import "./style/App.css";
 
-var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
-var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+var spotify_client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+var spotify_client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 
 function App() {
   const [token, setToken] = useState("");
   const [refreshToken, setRefreshToken] = useState("");
+  const [expiresIn, setExpiresIn] = useState(3600);
 
   useEffect(() => {
     async function getToken() {
       const response = await fetch("/auth/token");
       const json = await response.json();
-      console.log("getToken() json: ", json);
       setToken(json.access_token);
       setRefreshToken(json.refresh_token);
     }
 
-    const refreshAccessToken = () => {
-      console.log(
-        `spotify_client_secret: ${spotify_client_secret} | spotify_client_id: ${spotify_client_id}`
-      );
-      if (refreshToken != "") {
-        fetch("https://accounts.spotify.com/api/token", {
-          method: "POST",
-          body: new URLSearchParams({
-            refresh_token: refreshToken,
-            grant_type: "refresh_token",
-          }),
-          headers: {
-            Authorization:
-              "Basic " +
-              Buffer.from(
-                spotify_client_id + ":" + spotify_client_secret
-              ).toString("base64"),
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => console.log("returned data; ", data));
-      }
-    };
-
     console;
     getToken();
-    //setTimeout(refreshToken(), refreshAccessToken.expiresIn * 1000);
-    setTimeout(refreshAccessToken(), 60000);
+    //startTimer();
+    setInterval(() => refreshAccessToken(), expiresIn * 1000);
   }, []);
+
+  const refreshAccessToken = () => {
+    console.log("run refresh");
+    if (refreshToken != "") {
+      console.log("refreshAccessToken run");
+      fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        body: new URLSearchParams({
+          refresh_token: refreshToken,
+          grant_type: "refresh_token",
+        }),
+        headers: {
+          Authorization:
+            "Basic " +
+            Buffer.from(
+              spotify_client_id + ":" + spotify_client_secret
+            ).toString("base64"),
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("returned data; ", data);
+          setToken(data.access_token);
+          setExpiresIn(data.expires_in);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
     <>
