4c9fff41cfaeb16b72665487d7f6c8ff3fa7d34f
Josh <Josh.fryer2@gmail.com>
2021-10-14 00:19:23 +0100

    server index fix

--- server/index.js
+++ server/index.js
@@ -119,7 +119,7 @@ app.get("/auth/new-token", (req, res) => {
 // serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("../build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
